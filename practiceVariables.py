359e16e3580aaca5db4bd14cf60c1f38c42b6a59
lezlyv <57326531+lezlyv@users.noreply.github.com>
2019-11-30 10:23:18 -0600

    Initial commit

---
+++ practiceVariables.py
@@ -0,0 +1,95 @@
+'''
+Created on Nov 29, 2019
+
+@author: ITAUser
+'''
+
+name = 1234
+
+print(name)
+
+a = 4
+
+print(a)
+
+b = 2.3
+
+print(b)
+
+c = "abc"
+
+print(c)
+
+d = True  
+
+print(d)
+
+listString = ["string", "string2", "string 3"]
+
+listInt = [1, 2, 3]
+
+listFloat = [1.1, 1.2, 1.3]
+
+listBoolean = [True, False, True]
+
+listList = [[1, 2, 3], [1.1, 1.2, 1.3], ["1", "2", "3"]]
+
+listMulti = [1, 1.1, "1", True]
+
+varA= 4
+
+print(varA)
+
+vara = 5
+
+print(vara)
+
+a = 3
+
+print(a)
+
+a = 5
+
+print(a)
+
+a = b
+
+print(a)
+
+A = 4
+
+Var = 4
+
+vAr = 3
+
+varB = Var
+
+listTest = [1, 2, 3]
+
+zeroIndex = listTest[0]
+
+firstIndex = listTest[1]
+
+secondIndex = listTest[2]
+
+print(zeroIndex)
+print(firstIndex)
+print(secondIndex)
+
+name = "lezly"
+
+print(name[0])
+      
+print(name[0:3])
+
+print(name[3:])
+
+print(name[:4])
+
+print(name[0:])
+
+print(name)
+
+
+
+
